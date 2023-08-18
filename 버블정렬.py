5dd9bd2cfb6bcd265fc44aef718034195c2072de
SETPROC <126319049+SETPROC@users.noreply.github.com>
2023-08-18 15:53:28 +0900

    Update 버블정렬.py

--- 버블정렬.py
+++ 버블정렬.py
@@ -2,7 +2,7 @@ def bubble_sort(a):
 	n = len(a)
 	while True:
 		changed = False
-		for i in rnage(0,n-1):
+		for i in range(0,n-1):
 			if a[i] > a[i+1]:
 				print(a)
 				a[i],a[i+1] = a[i+1] , a[i]
